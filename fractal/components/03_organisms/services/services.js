ac4dc97bac27983a0ab010ca845e267037838404
jeromehaas <hello@jeromehaas.ch>
2022-07-25 16:31:37 +0200

    Setup: functionallity for active links in services component

--- fractal/components/03_organisms/services/services.js
+++ fractal/components/03_organisms/services/services.js
@@ -7,16 +7,21 @@ class Services {
 
 	constructor() {
 		this.name = 'services';
-		this.links = document.querySelectorAll('.services .sub-navigation__item');
+		this.elements = {
+			links: document.querySelectorAll('.services .sub-navigation__item'),
+			articles: document.querySelectorAll('.services .container__article'),
+		};
+
 	};
 	
 	init = () => {
 		if (!document.querySelector(`.js-${this.name}`)) return false;
-		this.setupScrollTrigger()
+		this.setupScrollTrigger();
+		this.setupLinkObserver();
 	};
 
 	setupScrollTrigger = () => {
-		const links = gsap.utils.toArray(this.links);
+		const links = gsap.utils.toArray(this.elements.links);
 		links.forEach((link) => {
 			if (link.hash) {
 				link.addEventListener('click', (event) => this.scrollToSection(event, link.hash));
@@ -28,6 +33,37 @@ class Services {
 		event.preventDefault();
 		gsap.to( window, { scrollTo: hash, duration: 1.3, ease: 'Power4.easeInOut' });
 	};
+
+	setupLinkObserver = () => {
+		const articles = gsap.utils.toArray(this.elements.articles);
+		const links = gsap.utils.toArray(this.elements.links);
+
+		console.log(articles);
+		articles.forEach((article, i) =>  {
+			ScrollTrigger.create({
+				trigger: article, 
+				start: "top 75%",
+				end: "top 75%",
+				markers: false,
+				onEnter: () => {
+					this.removeAcvitveLink();
+					this.setActiveLink(this.elements.links[i]);
+				},
+				onEnterBack: () => {
+					this.removeAcvitveLink();
+					this.setActiveLink(links[i - 1])	
+				}
+			});
+		});
+	};
+
+	removeAcvitveLink = () => {
+		this.elements.links.forEach((link) => link.classList.remove('sub-navigation__item--active'));
+	};
+
+	setActiveLink = (link) => {
+		if (link) link.classList.add('sub-navigation__item--active');
+	}
 
 };
 
